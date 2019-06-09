5fc071c069f0611250d23ce23a55ac5821ecf245
Kriyszig <lelouch.cpp@gmail.com>
2019-06-09 22:29:51 +0530

    convertTo for Axis to convert type of Axis.data
    * This would be necessary for column binary ops

--- source/magpie/axis.d
+++ source/magpie/axis.d
@@ -13,4 +13,46 @@ struct Axis(T...)
 
     /// Data being returned by the DataFrame
     AxisType data;
+
+    auto convertTo(U...)() @property
+        if(U.length == T.length)
+    {
+        import std.conv: to;
+        static if(U.length == 1)
+        {
+            Axis!U ret;
+            ret.data = to!(U[0])(data);
+            return ret;
+        }
+        else
+        {
+            Axis!U ret;
+            static foreach(i; 0 .. U.length)
+                ret.data[i] = to!(U[i])(data[i]);
+            return ret;
+        }
+    }
+}
+
+// Covert a probable column
+unittest
+{
+    Axis!(int[]) column;
+    column.data = [1,2,3,4];
+    auto res = column.convertTo!(double[]);
+    assert(res.data == [1.0, 2.0, 3.0, 4.0]);
+    assert(is(typeof(res.data) == double[]));
+}
+
+// Convert a probable row
+unittest
+{
+    Axis!(int, double) row;
+    row.data[0] = 12;
+    row.data[1] = 14.7;
+    auto res = row.convertTo!(double, int);
+    assert(res.data[0] == 12.0);
+    assert(res.data[1] == 14);
+    assert(is(typeof(res.data[0]) == double));
+    assert(is(typeof(res.data[1]) == int));
 }
