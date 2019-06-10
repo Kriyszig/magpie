faf54b2dc1551d5c423b67c655aaedb69e1b6497
Kriyszig <lelouch.cpp@gmail.com>
2019-06-10 15:07:30 +0530

    Addition Op on Axis
    * Addition operation on Axis - both for row and column like

--- source/magpie/axis.d
+++ source/magpie/axis.d
@@ -37,6 +37,36 @@ struct Axis(T...)
             return ret;
         }
     }
+
+    /++
+    Binary Operations on DataFrame row/column
+    +/
+    Axis!T opBinary(string op, U...)(Axis!U rhs)
+        if(U.length == T.length)
+    {
+        Axis!T ret;
+        static if(op == "+")
+        {
+            static if(U.length == 1)
+            {
+                assert(data.length == rhs.data.length, "Size mismatch");
+
+                foreach(i; 0 .. data.length)
+                    ret.data ~= data[i] + rhs.data[i];
+
+                return ret;                
+            }
+            else
+            {
+                static foreach(i; 0 .. U.length)
+                    ret.data[i] = data[i] + rhs.data[i];
+
+                return ret;
+            }
+        }
+
+        assert(0);
+    }
 }
 
 // Covert a probable column
@@ -60,4 +90,51 @@ unittest
     assert(res.data[1] == 14);
     assert(is(typeof(res.data[0]) == double));
     assert(is(typeof(res.data[1]) == int));
+}
+
+// Addition operation on multiple column
+unittest
+{
+    Axis!(double[]) a1;
+    a1.data = [1,2,3.7,4];
+    Axis!(int[]) a2;
+    a2.data = [1,2,3,4];
+    Axis!(double[]) a3;
+    a3.data = [1,2,3.7,4];
+    Axis!(double[]) a4;
+    a4.data = [1,2,3,4];
+
+    assert((a1 + a2).data == [2, 4, 6.7, 8]);
+    assert(a1.data == [1,2,3.7,4]);
+    assert(a2.data == [1,2,3,4]);
+    assert((a1 + a2 + a3 + a4).data == [4, 8, 13.4, 16]);
+}
+
+// Addition operation on multiple rows
+unittest
+{
+    Axis!(int, double) a1;
+    a1.data[0] = 1;
+    a1.data[1] = 1.786;
+
+    Axis!(int, int) a2;
+    a2.data[0] = 1;
+    a2.data[1] = 1;
+
+    Axis!(int, double) a3;
+    a3.data[0] = 1;
+    a3.data[1] = 1.786;
+
+    Axis!(int, double) a4;
+    a4.data[0] = 1;
+    a4.data[1] = 1;
+
+    import std.math: approxEqual;
+    auto res = a1 + a2;
+    assert(res.data[0] == 2);
+    assert(approxEqual(res.data[1], 2.786, 1e-3));
+
+    auto res2 = a1 + a2 + a3 + a4;
+    assert(res2.data[0] == 4);
+    assert(approxEqual(res2.data[1], 5.572, 1e-3));
 }
